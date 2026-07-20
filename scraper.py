e5e89a13ee2b39b2ba0f6d7692eded862cc66d68
mac <2503022050@student.beykent.edu.tr>
2026-07-20 15:22:40 +0300

    scraper: RSS ve XML okuma adımı eklendi

---
+++ scraper.py
@@ -0,0 +1,11 @@
+def rss_tara(url_listesi):
+    haberler = []
+
+    for url in url_listesi:
+        try:
+            pass
+        except Exception as x:
+
+            print(f"Hata oluştu ({url}): {x}")
+
+    return haberler
