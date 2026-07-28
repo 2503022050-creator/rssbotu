1eec92ff57d725fb6dec03015dfe868e40289b37
mac <2503022050@student.beykent.edu.tr>
2026-07-28 10:47:53 +0300

    feat: scraper modulu tamamlandi

--- scraper.py
+++ scraper.py
@@ -1,11 +1,25 @@
-def rss_tara(url_listesi):
+import requests
+from bs4 import BeautifulSoup
+
+
+def rss_tara(url_list):
     haberler = []
 
-    for url in url_listesi:
+    for url in url_list:
         try:
-            pass
-        except Exception as x:
+            cevap = requests.get(url, timeout=5)
+            xml_icerik = BeautifulSoup(cevap.text, features="xml")
+            haber_kutulari = xml_icerik.find_all("item")
 
-            print(f"Hata oluştu ({url}): {x}")
+            for haber in haber_kutulari:
+                baslik = haber.title.text if haber.title else "Başlık yok"
+                link = haber.link.text if haber.link else "Link yok"
+
+                haberler.append({
+                    "link": link,
+                    "baslik": baslik
+                })
+        except Exception as e:
+            print(f"Hata {url} adresinden veri çekilemedi: {e}")
 
     return haberler
